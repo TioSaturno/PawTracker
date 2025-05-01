74e586bdfd358f6b9610d7730a47254f420f47d0
ferbbrrzzgg <fernanda.barraza@mail.udp.cl>
2025-05-01 19:39:18 -0400

    cambios en app.js

--- App.js
+++ App.js
@@ -1,23 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { useEffect, useState} from "react"
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, Alert, Platform } from "react-native"
+//import MAPA 
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Home, Map, Settings, Bell } from "lucide-react-native"
+import * as Notifications from 'expo-notifications'
+import axios from "axios" 
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { ThresholdSettings } from "./components/ThresholdSettings"
+import { DeviceStatus } from "./components/DeviceStatus"
+import { NotificationHistory } from "./components/NotificationHistory"
+
+Notifications.setNotificacionesHandler({
+  handleNotification: async () => ({
+    notificacionAlerta: true, 
+    notificacionSonido: true, 
+    notificacionBadge: true, 
+  }),
+})
+
+const Tab = createBottomTabNavigator()
+
+//URL del servidor ESP32
+const API_URL = ""
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Image source = {{ uri: "" }}
-        style = {{ width: 503, heigth: 758 }}
-      />
-      <StatusBar style="auto" />
-      <Text>TUNG TUNG TUNG SAHUR</Text>
-    </View>
-  );
-}
+  const [expoPushToken, setExpoPushToken] = useState("")
+  const [deviceData, setDeviceData] = useState({
+    latitude: 0,
+    longitude: 0,
+    rssi: 0,
+    mode: "sleep", 
+    lastUpdate: "",
+  })
+  const [customThreshold, setCustomThreshold] = useState () //aca deberia estar la función del umbral personalizado 
+  const [fixedThreshold] = useState(-100)
+  const [NotificationHistory, setNotificationHistory] = useState([])
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect() => {
+    registerForPushNotificationAsync().then((token) => setExpoPushToken(token))
+    
+    loadCustomThreshold()
+
+    const notificationListener = 
+    Notification.addNotificationReceivedListener((notification) => {
+      const notificationData = notification.request.content 
+      setNotificationHistory((prev) => [
+        {
+          id: notification.request.identifier, 
+          title: notificationData.title, 
+          body: notificationData.body, 
+          time: new Date().toLocaleString(),
+        }
+        ...prev,
+      ])
+    })
+
+    const interval = setInterval(() => {
+      fetchDeviceData()
+    }, 30000)
+
+    return () => {
+      clearInterval(interval)
+      Notifications.removeNotificationSubscripcion(notificationListerner)
+    }
+  }, [])
+  }
+}
