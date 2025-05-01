ef9d2d5079b1ac4da189a613a54c3f07821d0bcc
Cristobal Caro Hoffmann <cristobal.caro@mail.udp.cl>
2025-05-01 19:51:21 -0400

    Componente Para el Mapa y Funcionalidad del mapa

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { View, StyleSheet, Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Main } from "./components/main"
 export default function App() {
-  return (
+  return ( 
+    <SafeAreaProvider>
     <View style={styles.container}>
-      <Image source = {{ uri: "https://static.wikia.nocookie.net/omegia/images/4/49/TungTungTungSahur.jpg/revision/latest?cb=20250331233111&path-prefix=es" }}
-        style = {{ width: 503, heigth: 758 }}
-      />
-      <StatusBar style="auto" />
-      <Text>TUNG TUNG TUNG SAHUR</Text>
+      <StatusBar style="ligth" />
+      <Main /> 
     </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: 'white',
+  }
 });
---
+++ components/main.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { StyleSheet, View} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+export function Main() {
+   const insets = useSafeAreaInsets(); 
+   const [DogLocation, SetDogLocation] = React.useState({
+    latitude: -33.512863,
+    longitude: -70.597444   
+   })
+   /*const [isConected, SetIsConected] = React.useState(true);
+   const navigation = useNavigation()
+   useEffect(() => {
+    const db= getFireStore();
+    const locationRef = collection (db, "locations")
+    const q = query(locationRef, orderby ("timestamp", "desc"), limit(1));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      if(!querySnapshot.empty) {
+        const locationData = querySnapshot.docs[0].data();
+        SetDogLocation({
+          latitude: locationData.latitude,
+          longitude: locationData.longitude
+        })
+        SetIsConected(locationData.isConected)
+      }
+    })
+    return () => unsubscribe();
+  }, []);
+  */
+ //Todo esto sera cuando firestore este integrado
+  return ( 
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom}}> 
+      <MapView
+      style ={styles.map}
+        initialRegion={{
+            latitude: DogLocation.latitude, 
+            longitude: DogLocation.longitude,
+            longitudeDelta: 0.01,
+            latitudeDelta: 0.01
+        }}  
+      /> 
+      <Marker
+        coordinate={DogLocation}
+        title={"Tu perro"}
+        description={isConected ? "Conectado a Wifi" : "Usando GPS"}
+        pinColor={"blue"}></Marker>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%"
+  }
+}); 
